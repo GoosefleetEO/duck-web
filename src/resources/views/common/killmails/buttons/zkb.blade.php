e3b08fa3e90a01863d7de394506031b662c27b20
Tama Gotchi <47679051+digital-pet@users.noreply.github.com>
2022-08-30 19:52:36 -0400

    Add link to individual killmail rather than zkill

--- src/resources/views/common/killmails/buttons/zkb.blade.php
+++ src/resources/views/common/killmails/buttons/zkb.blade.php
@@ -1,4 +1,12 @@
-<a href="https://zkillboard.com/kill/{{ $row->killmail_id }}/" target="_blank" class="text-muted">
+<a 
+        @if(request()->character)
+        href="{{ route('character.view.killmail', ['character' => request()->character, 'killmail' => $row]) }}"
+        @endif
+        @if(request()->corporation)
+        href="{{ route('corporation.view.killmail', ['corporation' => request()->corporation, 'killmail' => $row]) }}"
+        @endif
+target="_blank" class="text-muted">
   <i class="fas fa-external-link-alt"></i>
-  zKB
+  Link
 </a>
+<!-- href="https://zkillboard.com/kill/{{ $row->killmail_id }}/" -->
