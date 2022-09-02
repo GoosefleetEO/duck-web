caebc30d3df99be8e6ff72420ea3fa0a42f3459e
Tama Gotchi <47679051+digital-pet@users.noreply.github.com>
2022-09-02 19:13:32 -0400

    Remove visual clutter
    Removed status graphs that were A) backwards and B) utterly irrelevant to users.

--- src/resources/views/home.blade.php
+++ src/resources/views/home.blade.php
@@ -103,44 +103,6 @@
   </div>
 
   <div class="row">
-    
-    <!-- player count -->
-    <div class="col-xs-12 col-sm-6">
-
-      <div class="card">
-        <div class="card-header border-0">
-          <div class="d-flex justify-content-between">
-            <h3 class="card-title">{{ trans('web::seat.concurrent_player_count') }}</h3>
-          </div>
-        </div>
-        <div class="card-body">
-          <div class="position-relative mb-4">
-            <canvas id="serverstatus" height="150" width="1110"></canvas>
-          </div>
-        </div>
-        <!-- /.box-body -->
-      </div>
-    </div>
-
-    <!-- response times -->
-    <div class="col-xs-12 col-sm-6">
-
-      <div class="card">
-        <div class="card-header border-0">
-          <div class="d-flex justify-content-between">
-            <h3 class="card-title">{{ trans('web::seat.esi_response_time') }}</h3>
-          </div>
-        </div>
-        <div class="card-body">
-          <div class="position-relative mb-4">
-            <canvas id="serverresponse" height="150" width="1110"></canvas>
-          </div>
-        </div>
-        <!-- /.box-body -->
-      </div>
-    </div>
-    
-
     <!-- skills graphs -->
     @if(auth()->user()->name != 'admin')
       <div class="col-xs-12 col-sm-6">
@@ -191,44 +153,6 @@
 
 @push('javascript')
   <script type="text/javascript">
-
-    // Player Count
-    $.get("{{ route('home.chart.serverstatus') }}", function (data) {
-
-      new Chart($("canvas#serverstatus"), {
-        type   : 'line',
-        data   : data,
-        options: {
-          legend: {
-            display: false
-          },
-          scales: {
-            xAxes: [{
-              display: false
-            }]
-          }
-        }
-      });
-    });
-
-    // Esi Response Times
-    $.get("{{ route('home.chart.serverresponse') }}", function (data) {
-
-      new Chart($("canvas#serverresponse"), {
-        type   : 'line',
-        data   : data,
-        options: {
-          legend: {
-            display: false
-          },
-          scales: {
-            xAxes: [{
-              display: false
-            }]
-          }
-        }
-      });
-    });
 
     if ($('canvas#skills-level').length)
       $.get("{{ route('character.view.skills.graph.level', ['character' => auth()->user()->main_character]) }}", function (data) {
