9c14a76bdd8c816e25e6f50654b26df140759f58
Tama Gotchi <47679051+digital-pet@users.noreply.github.com>
2022-09-02 16:03:10 -0400

    Making sure nothing else depends on these filters

--- src/resources/views/common/contacts/buttons/filters.blade.php
+++ src/resources/views/common/contacts/buttons/filters.blade.php
@@ -1,43 +1,5 @@
 <div class="mb-3">
-  <div class="btn-group d-flex">
-    <button data-filter-field="standing" data-filter-value="-10" class="btn btn-light dt-filters active">
-      -10
-    </button>
-    <button data-filter-field="standing" data-filter-value="-6.9" class="btn btn-light dt-filters active">
-      -6.9
-    </button>
-    <button data-filter-field="standing" data-filter-value="-5" class="btn btn-light dt-filters active">
-      -5
-    </button>
-    <button data-filter-field="standing" data-filter-value="0" class="btn btn-light dt-filters active">
-      0
-    </button>
-    <button data-filter-field="standing" data-filter-value="0.1" class="btn btn-light dt-filters active">
-      0.1
-    </button>
-    <button data-filter-field="standing" data-filter-value="5" class="btn btn-light dt-filters active">
-      5
-    </button>
-    <button data-filter-field="standing" data-filter-value="10" class="btn btn-light dt-filters active">
-      10
-    </button>
-  </div>
-</div>
-<div class="mb-3">
-  <div class="btn-group d-flex">
-    <button data-filter-field="category" data-filter-value="character" class="btn btn-light dt-filters active">
-      <i class="fas fa-user"></i>
-      Character
-    </button>
-    <button data-filter-field="category" data-filter-value="corporation" class="btn btn-light dt-filters active">
-      <i class="fas fa-building"></i>
-      Corporation
-    </button>
-    <button data-filter-field="category" data-filter-value="alliance" class="btn btn-light dt-filters active">
-      <i class="fas fa-star"></i>
-      Alliance
-    </button>
-  </div>
+<span style="color:red;background:rgba(255,255,255,128);">IF THIS SHOWS UP PING CORGSKI</span>
 </div>
 
 @push('javascript')
